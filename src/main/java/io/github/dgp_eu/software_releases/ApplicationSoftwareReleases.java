fd2ce11d45ef68f8980a6c62638ce15ab724764f
Daniel Popiniuc <danielpopiniuc@gmail.com>
2026-08-08 10:08:30 +0300

    removing unused import

--- src/main/java/io/github/dgp_eu/software_releases/ApplicationSoftwareReleases.java
+++ src/main/java/io/github/dgp_eu/software_releases/ApplicationSoftwareReleases.java
@@ -5,7 +5,6 @@ package io.github.dgp_eu.software_releases;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
